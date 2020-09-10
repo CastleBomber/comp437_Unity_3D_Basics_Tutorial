d1e6a1e05581b8341e19118676dc1677f3be3a44
CastleBomber <richie.ucsb@gmail.com>
2020-09-09 20:16:47 -0700

    done

---
+++ Assets/RandomMovements.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RandomMovements : MonoBehaviour {
+
+	public float speed = 3.0f;
+	private bool restartMovementOnNextSpaceHit = false;
+
+	void Update()
+	{
+		transform.Rotate(0, speed, 0);
+		if (Input.GetKey("space") && restartMovementOnNextSpaceHit == false)
+		{
+			speed = 0;
+			restartMovementOnNextSpaceHit = true;
+		}
+		else if (Input.GetKey("space") && restartMovementOnNextSpaceHit == true)
+		{
+			speed = 3.0f;
+			restartMovementOnNextSpaceHit = false;
+		}
+	}
+}
+ 
